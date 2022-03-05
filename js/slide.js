28f884f4e7c2c70f09b9b6bcc4319fc1eb4291c6
Dario <darioo_dario@hotmail.com>
2022-03-04 22:51:01 -0300

    criando classe

---
+++ js/slide.js
@@ -0,0 +1,25 @@
+export default class Slide {
+  constructor(slide, wrapper) {
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+  }
+
+  onStart(event) {
+   event.preventDefault();
+   console.log(this)
+  }
+
+  addSlideEvents() {
+    this.wrapper.addEventListener("mousedown", this.onStart);
+  }
+
+  // bindEvents() {
+  //   this.onStart() = this.onStart.bind(this);
+  // }
+
+  init() {
+    //this.bindEvents();
+    this.addSlideEvents();
+    return this;
+  }
+}
