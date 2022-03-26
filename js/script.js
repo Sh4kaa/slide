ded6d1aa64ae50b6f6d0cce15910af24ba3ee330
Dario <darioo_dario@hotmail.com>
2022-03-26 10:02:11 -0300

    adicionado slide controls

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-import {SlideNav} from './slide.js';
+import SlideNav from './slide.js';
 
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
-
-slide.addArrow('.prev', '.next')
-console.log(slide)
-
-slide.addControl();
+slide.addArrow('.prev', '.next');
+slide.addControl('.custom-controls');
